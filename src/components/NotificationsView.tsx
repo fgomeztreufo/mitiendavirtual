df1ae2d1cb9a0a04897313fed7d04d787318ce58
Felipe Alonso Gomez Treufo <felipe.gomezt@bci.cl>
2026-05-01 18:48:51 -0400

    a

--- src/components/NotificationsView.tsx
+++ src/components/NotificationsView.tsx
@@ -99,8 +99,12 @@ const script = document.createElement('script');
 script.src = "https://telegram.org/js/telegram-widget.js?22";
 script.setAttribute('data-telegram-login', 'Mitiendavirtualclbot');
 script.setAttribute('data-size', 'large');
-script.setAttribute('data-onauth', 'onTelegramAuth(user)'); // IMPORTANTE
+script.setAttribute('data-onauth', 'onTelegramAuth(user)');
 script.setAttribute('data-request-access', 'write');
+
+// Agrega esta línea para forzar que el widget use el dominio con www
+script.setAttribute('data-origin', 'https://www.mitiendavirtual.cl'); 
+
 script.async = true;
 document.getElementById('telegram-login-container')?.appendChild(script);
         },
